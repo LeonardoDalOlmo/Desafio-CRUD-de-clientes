e24afe75bf62b70583eddcd90121b5b3bcc5ebeb
Leonardo Dal Olmo <leo.dalolmoofc@gmail.com>
2025-07-11 23:26:56 -0300

    Finalizado Seed

--- projeto/projeto/src/main/java/com/aula/projeto/entities/Client.java
+++ projeto/projeto/src/main/java/com/aula/projeto/entities/Client.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 
 @Entity
 @Table(name = "tb_client")
